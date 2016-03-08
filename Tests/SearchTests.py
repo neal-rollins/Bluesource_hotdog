999e964b51c00b44f17eaefbfbf15af0e675a1b4
Matt Watson <matt.watson@orasi.com>
2016-03-08 16:01:07 -0500

    update

--- Tests/SearchTests.py
+++ Tests/SearchTests.py
@@ -11,24 +11,30 @@ class SearchTests(BaseTest):
 
     def test_contentDetailsElements(self):
 
-        self.driver.debug = False
+        self.driver.debug = True
         self.driver.get('https://the-internet.herokuapp.com/checkboxes')
 
         common = CommonPage(driver=self.driver, url=self.page_url)
         common.sync()
 
-        #Multiple ways of doing the same thing
+        #Find single element untyped
         headerText = common.find_element(By.CSS_SELECTOR, '.example h3')
         headerText = common.find_element(by=By.CSS_SELECTOR, value='.example h3')
         headerText = common.find('headerText')
         headerText.click()
 
-        #Multiple Ways of doing the same thing
+        #Find single element typed
         checkboxes = common.find_element(By.ID, 'checkboxes', Checkbox)
         checkboxes = common.find_element(by=By.ID, value='checkboxes', type=Checkboxes)
         checkboxes = common.Checkboxes
 
-        #Multiple Ways of doing the same thing
+        #Find multiple elements untyped
+        boxes = checkboxes.find_elements(By.CSS_SELECTOR, "input[type='checkbox']")
+        boxes = checkboxes.find_elements(by=By.CSS_SELECTOR, value="input[type='checkbox']")
+        boxes = checkboxes.finds('checkboxesLoc')
+        boxes = checkboxes.checkboxes
+
+        #Find multiple elements typed
         boxes = checkboxes.find_elements(By.CSS_SELECTOR, "input[type='checkbox']", Checkbox)
         boxes = checkboxes.find_elements(by=By.CSS_SELECTOR, value="input[type='checkbox']", type=Checkbox)
         boxes = checkboxes.finds('checkboxesLoc')
